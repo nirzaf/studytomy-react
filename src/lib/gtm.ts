0d575d0215d451ace14b1eaf2be1865ab784f58a
MOHAMED FAZRIN <mfmfazrin1986@gmail.com>
2024-11-20 20:48:27 +0300

    feat: Improve GTM and GA4 initialization (master)
    Improved type safety in GTM and GA4 initialization.  Added
    null check before inserting GTM script to prevent errors.
    Updated gtag function to accept variadic arguments for
    improved flexibility.

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -12,22 +12,23 @@ export const initGTM = () => {
     window.dataLayer = window.dataLayer || [];
 
     // Initialize GTM
-    (function(w,d,s,l,i){
+    (function(w: { [key: string]: any },d: Document,s: string,l: string,i: string){
       w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      j=d.createElement(s) as HTMLScriptElement,dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      if (f && f.parentNode) f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer',GTM_ID);
 
     // Initialize GA4
-    const script = document.createElement('script');
+    const script = document.createElement('script') as HTMLScriptElement;
     script.async = true;
     script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
     document.head.appendChild(script);
 
     window.dataLayer = window.dataLayer || [];
     function gtag(...args: any[]) {
-      window.dataLayer.push(arguments);
+      window.dataLayer.push(args);
     }
     gtag('js', new Date());
     gtag('config', GA_ID);
